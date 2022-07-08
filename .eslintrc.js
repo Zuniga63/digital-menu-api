68baf49f36d7edcea65e9e5b7df50278a1d68780
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-08 15:54:28 -0500

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+const RULES = {
+  OFF: 'off',
+  ERROR: 'error',
+  WARN: 'warn',
+};
+
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': RULES.ERROR,
+    'no-underscore-dangle': RULES.OFF,
+    'no-unused-vars': [RULES.ERROR, { argsIgnorePattern: '^_' }],
+    'import/extensions': [
+      RULES.ERROR,
+      'ignorePackages',
+      {
+        js: 'never',
+        ts: 'never',
+      },
+    ],
+  },
+  /* https://stackoverflow.com/questions/59265981/typescript-eslint-missing-file-extension-ts-import-extensions */
+  settings: {
+    'import/extensions': ['.js', '.ts'],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts'],
+      },
+    },
+  },
+};
