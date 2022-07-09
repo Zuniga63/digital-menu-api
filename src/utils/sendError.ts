5190857e6839e66a354fea81828f84fead364b3a
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-09 17:07:54 -0500

    update sendError for add info object

--- src/utils/sendError.ts
+++ src/utils/sendError.ts
@@ -1,5 +1,11 @@
 import { Response } from 'express';
 
+export interface IErrorInfo {
+  message: string;
+  ok: boolean;
+  error?: any;
+  validationErrors?: object;
+}
 /**
  * Response Code
  * 400: Bad Request
@@ -13,16 +19,21 @@ import { Response } from 'express';
  * 511 Network Authentication Required
  */
 export default function sendError(error: any, res: Response) {
-  const ok = false;
   const { name: errorName, message }: { name: string; message: string } = error;
   let code: number = 500;
+  const info: IErrorInfo = {
+    message,
+    ok: false,
+  };
 
   if (errorName === 'ValidationError' || errorName === 'InvalidSignInError') {
+    info.message = 'Error de validación';
+    info.validationErrors = error.errors;
     code = 400;
   } else if (errorName === 'AuthError') code = 401;
   else if (errorName === 'NotFoundError') code = 404;
   // eslint-disable-next-line no-console
   else console.log(error);
 
-  res.status(code).json({ message, ok });
+  res.status(code).json(info);
 }
