f3fcdbfeacb1eb5baefe3fa6dd3f5a0fc149aec7
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-10 08:50:39 -0500

    add cloudinary presets script

---
+++ src/utils/cloudinaryPresets/createOptionItemPreset.ts
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+import { v2 as cloudinary } from 'cloudinary';
+
+export default async function createOptionItemPreset(): Promise<void> {
+  try {
+    const preset = await cloudinary.api.create_upload_preset({
+      name: 'option_item_preset',
+      folder: 'digital-menu/option_items',
+      resource_type: 'image',
+      allowed_formats: 'jpg, png, gif, webp, bmp, jpe, jpeg',
+      access_mode: 'public',
+      unique_filename: true,
+      auto_tagging: 0.7,
+      overwrite: true,
+
+      transformation: [
+        {
+          width: 200,
+          height: 200,
+          crop: 'thumb',
+        },
+      ],
+    });
+
+    console.log(preset);
+  } catch (error: any) {
+    console.error(error.error.message);
+  }
+}
