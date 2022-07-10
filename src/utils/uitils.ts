573ef64c2ef523867081ecea8c7a22c1c59c9c48
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-10 10:37:09 -0500

    add util for create slug

--- src/utils/uitils.ts
+++ src/utils/uitils.ts
@@ -1,3 +1,19 @@
 export const emailRegex = /^[^@]+@[^@]+.[^@]+$/;
 export const strongPass =
   /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^A-Za-z0-9])(?=.{8,})/;
+
+/**
+ * Normaliza el texto, cambia los espacios por (-) ademas de eliminar
+ * los signos de puntucacion.
+ * @param {string} text Texto a normalizar
+ * @returns String
+ */
+export const createSlug = (text: string) =>
+  text
+    ? text
+        .trim()
+        .toLocaleLowerCase()
+        .replace(/\s/gi, '-')
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+    : '';
