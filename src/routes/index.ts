a52556c1f79cb681be446896584ea73de7545f62
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-07-22 08:14:38 -0500

    add controller for option sets

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import AuthRouter from './Auth.router';
 import ProductCategoryRouter from './ProductCategory.router';
 import ProductRouter from './Product.router';
+import OptionSetRouter from './OptionSet.router';
 
 const rootRouter = Router();
 
 rootRouter.use('/auth', AuthRouter);
 rootRouter.use('/product-categories', ProductCategoryRouter);
 rootRouter.use('/products', ProductRouter);
+rootRouter.use('/set-options', OptionSetRouter);
 
 export default rootRouter;
